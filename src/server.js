85cbd8be0e4f59f2dcda2a88d30850899c51c07a
Cluster-Taek <yst811@naver.com>
2021-08-01 17:34:33 +0900

    Kakao Map API 반영

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ app.use(favicon(path.join(__dirname, 'views', 'img', 'common', 'favicon.ico')));
 app.use("/public", express.static(__dirname + "/public"))
 app.use("/views", express.static(__dirname + "/views"))
 app.get("/", (req, res) => res.render("home"));
+app.get("/intro/intro/introInfo.hs", (req, res) => res.render("intro"))
+app.get("/intro/map/mapInfo.hs", (req, res) => res.render("map"))
 app.get("/*", (req, res) => res.redirect("/"))
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
